3b65bcf241345dd5bba09107dcbafb57014e274a
viktorgp <viktor.gonzalezp98@gmail.com>
2023-09-22 00:09:26 -0300

    post delete changes

--- app/posts/[id]/comments/loading.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./loading.module.css"
-export default function Loading(){
-    return (
-        <p className={styles.button}></p>
-    )
-}
--- app/posts/layout.jsx
+++
@@ -1,9 +0,0 @@
-import { Counter } from "./Counter"
-export default function PostLayout ({children}){
-    return (
-        <div>
-            <small>Home &bull; Posts</small>
-            {children}
-        </div>
-    )
-}
