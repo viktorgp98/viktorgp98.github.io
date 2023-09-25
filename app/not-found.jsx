4fef9cf1c8e2bf25b4e55a082fb78ce40facd3da
viktorgp <viktor.gonzalezp98@gmail.com>
2023-09-25 17:44:37 -0300

    pagina not found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,12 +1,27 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Página no encontrada</h2>
-      <p>Volver a la página principal</p>
-      <p>
-         <Link href="/">Inicio</Link>
-      </p>
+    <div id={styles.oopss}>
+      <div id={styles.error_text}>
+        <img
+          src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+          alt="404"
+        />
+        <span>PAGINA NO ENCONTRADA</span>
+        <p class={styles.p_a}>. La pagina que buscabas no existe</p>
+        <a href="/" class={styles.back}>
+          ... Volver al Inicio
+        </a>
+      </div>
     </div>
+    // <div className={styles.container}>
+    //   <h2>Error 404: Página no encontrada</h2>
+    //   <p>Volver a la página principal</p>
+    //   <p>
+    //     <Link href="/">Inicio</Link>
+    //   </p>
+    // </div>
   );
 }
