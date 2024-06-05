96de78e20c5ec7099e2a63aac0ba57170c4b0b27
viktorgp <viktor.gonzalezp98@gmail.com>
2024-06-05 02:35:02 -0400

    caca

--- next.config.js
+++ next.config.js
@@ -1,25 +1,43 @@
 /**
  * @type {import('next').NextConfig}
  */
-
-
-let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
-
 const nextConfig = {
-  
-     output:  process.env.NODE_ENV !== "production" ? undefined: "export",
-       images: {
-         unoptimized: envImageUnoptimize,
-         loader: 'imgix',
-         path: '/',
-           remotePatterns: [
-             {
-               protocol: 'https',
-               hostname: 'firebasestorage.googleapis.com',
-             },
-           ],
-       }
-
-      }
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
 module.exports = nextConfig
-// export default nextConfig;
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+
+
+// let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+
+// const nextConfig = {
+  
+//      output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+//        images: {
+//          unoptimized: envImageUnoptimize,
+//          loader: 'imgix',
+//          path: '/',
+//            remotePatterns: [
+//              {
+//                protocol: 'https',
+//                hostname: 'firebasestorage.googleapis.com',
+//              },
+//            ],
+//        }
+
+//       }
+// module.exports = nextConfig
+// // export default nextConfig;
