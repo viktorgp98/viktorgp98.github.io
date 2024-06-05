809b229428a344b60f63758020f7fc2738df5de2
viktorgp <viktor.gonzalezp98@gmail.com>
2024-06-05 03:17:52 -0400

    ca

--- next.config.js
+++ next.config.js
@@ -1,55 +1,27 @@
-/**
- * @type {import('next').NextConfig}
- */let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
-const nextConfig = {
-  output: "export",
-  images: {
-              unoptimized: envImageUnoptimize,
-              loader: 'imgix',
-              path: '/',
-                remotePatterns: [
-                  {
-                    protocol: 'https',
-                    hostname: 'firebasestorage.googleapis.com',
-                  },
-                ],
-            }
-      ,    
-      pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'], 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
 
 
-// let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+ /**
+  * @type {import('next').NextConfig}
+  */
 
-// const nextConfig = {
+
+ let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+
+ const nextConfig = {
   
-//      output:  process.env.NODE_ENV !== "production" ? undefined: "export",
-//        images: {
-//          unoptimized: envImageUnoptimize,
-//          loader: 'imgix',
-//          path: '/',
-//            remotePatterns: [
-//              {
-//                protocol: 'https',
-//                hostname: 'firebasestorage.googleapis.com',
-//              },
-//            ],
-//        }
+      output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+        images: {
+          unoptimized: envImageUnoptimize,
+          loader: 'imgix',
+          path: '/',
+            remotePatterns: [
+              {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+              },
+            ],
+        }
 
-//       }
-// module.exports = nextConfig
-// // export default nextConfig;
+       }
+//  module.exports = nextConfig
+  export default nextConfig;
