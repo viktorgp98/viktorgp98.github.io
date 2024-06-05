4d3a48153900fbe082e02465c203a69e6420dfd7
viktorgp <viktor.gonzalezp98@gmail.com>
2024-06-05 01:42:05 -0400

    nextcioss

--- next.config.mjs
+++ next.config.mjs
@@ -4,22 +4,25 @@
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 
 const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    }
+    module.exports = nextConfig
   
   
-  
-    output:  process.env.NODE_ENV !== "production" ? undefined: "export",
-      images: {
-        unoptimized: envImageUnoptimize,
-        loader: 'imgix',
-        path: '/',
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'firebasestorage.googleapis.com',
-            },
-          ],
-      }
-}
+    // output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+    //   images: {
+    //     unoptimized: envImageUnoptimize,
+    //     loader: 'imgix',
+    //     path: '/',
+    //       remotePatterns: [
+    //         {
+    //           protocol: 'https',
+    //           hostname: 'firebasestorage.googleapis.com',
+    //         },
+    //       ],
+    //   }
+
 
 // module.exports = nextConfig
-export default nextConfig;
+// export default nextConfig;
