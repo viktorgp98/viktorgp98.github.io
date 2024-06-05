b988e37e411ef73e63aad8c74e490ac220dd1625
viktorgp <viktor.gonzalezp98@gmail.com>
2024-06-05 01:18:01 -0400

    nextconfig change

--- next.config.js
+++ next.config.js
@@ -4,18 +4,22 @@
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 
 const nextConfig = {
-  output:  process.env.NODE_ENV !== "production" ? undefined: "export",
-    images: {
-      unoptimized: envImageUnoptimize,
-      loader: 'imgix',
-      path: '/',
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'firebasestorage.googleapis.com',
-          },
-        ],
-    }
+  
+  
+  
+    output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+      images: {
+        unoptimized: envImageUnoptimize,
+        loader: 'imgix',
+        path: '/',
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'firebasestorage.googleapis.com',
+            },
+          ],
+      }
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+export default nextConfig;
