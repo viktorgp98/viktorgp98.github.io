27964045b26a2473ca3a49c91ed3816040ffbc38
viktorgp <viktor.gonzalezp98@gmail.com>
2024-06-05 01:55:59 -0400

    nextconfig chages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,23 @@
 /**
  * @type {import('next').NextConfig}
  */
-let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+
 
 const nextConfig = {
   
   reactStrictMode: true,
-
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  
  // Add basePath
   basePath: 'https://viktorgp98.github.io/',
-
+}
 }
     module.exports = nextConfig
   
-  
+  //let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
     // output:  process.env.NODE_ENV !== "production" ? undefined: "export",
     //   images: {
     //     unoptimized: envImageUnoptimize,
