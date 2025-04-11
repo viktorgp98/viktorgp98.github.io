c0b6fcb490331ca9f89c62964132487b8d42aca5
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-04-10 23:03:56 -0400

    Add experiences data

---
+++ src/consts/experiences.ts
@@ -0,0 +1,49 @@
+export const EXPERIENCES = [
+  {
+    id: "altue",
+    color: "green",
+    company: "Semillas Altue",
+    location: "Quinta de Tilcoco, Chile",
+    title: "Desarrollador de software",
+    image: "/images/logos/altue-logo.webp",
+    start: "Nov 2024",
+    end: "Mar 2025",
+    description:
+      "Encargado de desarrollo de más de 10 módulos para sistema de proceso de tratado de semillas y hortazilas. Utilizando React, NodeJS y MSSQL",
+    // techStack:['react', 'nodejs', 'mssql'],
+    techStack:[{name:'react'},{name:'nodejs'},{name:'mssql'},],
+  },
+  {
+    id: "gamersup",
+    color: "blue",
+    company: "Semillas Altue",
+    location: "Rancagua, Chile",
+    title: "Administrador",
+    image: "/images/logos/gamersup-logo.webp",
+    start: "Abr 2022",
+    end: "Jun 2023",
+    description:
+   `Encargado de la administración del E-Commerce de la tienda,
+mediante el CMS Wordpress. Además de la toma de pedidos
+online y mayoristas. 
+`,
+    techStack:[{name:'wordpress'}],
+    // techStack:['react', 'node', 'mssql'],
+  },
+  {
+    id: "bigbuda",
+    color: "white",
+    company: "Semillas Altue",
+    location: "Las Condes, Chile (Remoto)",
+    title: "Web Developer (trainee)",
+    image: "/images/logos/bigbuda-logo.webp",
+    start: "Feb 2021",
+    end: "Jul 2021",
+    description:
+    `Encargado del desarrollo de Ecommerce’s y sitios web corporativos
+mediante el CMS, Wordpress. También realizar soporte técnico y
+administración de páginas web`,
+    // techStack:['react', 'nodejs', 'mssql'],
+    techStack:[{name:'wordpress'},{name:'html'},{name:'css'},{name:'javascript'},],
+  },
+];
