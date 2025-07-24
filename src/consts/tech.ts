cb12da045be99bea0e73fb75960033639630d0e2
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-07-24 18:32:03 -0400

    Update dependencies and add new project assets; refactor imports and enhance tech stack representation

--- src/consts/tech.ts
+++ src/consts/tech.ts
@@ -1,66 +1,71 @@
 export const TECHS = [
-    {
-      name: "frontend",
-      techs: [
-        {
-          id: "react",
-          name: "React.js",
-        },
-        {
-          id: "astro",
-          name: "Astro.js",
-        },
-        {
-          id: "css",
-          name: "CSS",
-        },
-        {
-          id: "tailwind",
-          name: "Tailwind",
-        },
-      ],
-    },
-    {
-      name: "backend",
-      techs: [
-        {
-          id: "javascript",
-          name: "JavaScript",
-        },
-        {
-          id: "expressjs",
-          name: "Express",
-        },
-        {
-          id: "python",
-          name: "Python",
-        },
-        {
-          id: "nodejs",
-          name: "NodeJS",
-        },
-        {
-          id: "php",
-          name: "PHP",
-        },
-      ],
-    },
-    {
-      name: "db & servicios",
-      techs: [
-          {
-            id: "mssql",
-            name: "MSSQL",
-          },
-        {
-          id: "firebase",
-          name: "Firebase",
-        },
-        {
-          id: "mysql",
-          name: "MySQL",
-        },
-        {id:"git", name:"Git"},{id:"wordpress", name:"Wordpress"},
-      ],
-    },
-  ];
+  {
+    name: "frontend",
+    techs: [
+      {
+        id: "react",
+        name: "React.js",
+      },
+      {
+        id: "native",
+        name: "React Native",
+      },
+      {
+        id: "astro",
+        name: "Astro.js",
+      },
+      {
+        id: "css",
+        name: "CSS",
+      },
+      {
+        id: "tailwind",
+        name: "Tailwind",
+      },
+    ],
+  },
+  {
+    name: "backend",
+    techs: [
+      {
+        id: "javascript",
+        name: "JavaScript",
+      },
+      {
+        id: "expressjs",
+        name: "Express",
+      },
+      {
+        id: "python",
+        name: "Python",
+      },
+      {
+        id: "nodejs",
+        name: "NodeJS",
+      },
+      {
+        id: "php",
+        name: "PHP",
+      },
+    ],
+  },
+  {
+    name: "db & servicios",
+    techs: [
+      {
+        id: "mssql",
+        name: "MSSQL",
+      },
+      {
+        id: "firebase",
+        name: "Firebase",
+      },
+      {
+        id: "mysql",
+        name: "MySQL",
+      },
+      { id: "git", name: "Git" },
+      { id: "wordpress", name: "Wordpress" },
+    ],
+  },
+];
